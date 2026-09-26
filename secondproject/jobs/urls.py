1ee8e07d0dceb1e4830673096e2a16cbb471abf2
Duc Harper <djharper757@gmail.com>
2026-09-26 03:56:19 -0400

    Committing some changes that I forgot to save

--- secondproject/jobs/urls.py
+++ secondproject/jobs/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='jobs.index'),
     path('<int:id>/', views.show, name='jobs.show'),
     path('<int:id>/apply/', views.apply, name='jobs.apply'),
+    path('manage/', views.manage, name='jobs.manage'),
+    path('create/', views.create, name='jobs.create'),
 ]
